ded3735125f1764284b957c86df1cfbf3c88ca0a
samuel <reynaldosamuel7@gmail.com>
2026-09-18 11:06:52 -0300

    \exercicio_01.js

--- aula2/conceito_tipos_de_dados.js
+++ aula2/conceito_tipos_de_dados.js
@@ -9,3 +9,24 @@
 //null (variavel sem valor)
 //object (objeto)
 //array (vetor)
+
+//string (texto)
+//uma string é um conjunto de caracteres, ou seja, um texto. no javascript, podemos criar uma string usando aspas simples ou aspas duplas.
+let nome = "Jarvis"
+let mensagem = 'ola mundo';
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "ola " + nome + "!";
+console.log(saudacao); // exibir "ola Jarvis!"
+
+//typeof
+// o typeof serve para descobrir o tipo de um valor ou variavel.
+let nomedois = "samuel"
+console.log(typeof nomedois); 
+
+let soma = 10 + 5; 
+console.log(soma)
+
+
---
+++ aula2/exercicio_01.js
@@ -0,0 +1,7 @@
+// atividade number 
+// crie uma variavel chama anoatual e outra chamada anonascimento.
+//calcule e exiba a idade no (A) console.
+let anoatual = 2026;
+let anonascimento = 2010;
+let idade = anoatual - anonascimento;
+console.log(idade);
